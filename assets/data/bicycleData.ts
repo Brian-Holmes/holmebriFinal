d84aca3134fbdfe1d1a42bf1a214a801b142deaf
Brian-Holmes <52579801+Brian-Holmes@users.noreply.github.com>
2019-08-16 12:53:08 -0400

    Add files via upload

---
+++ assets/data/bicycleData.ts
@@ -0,0 +1,26 @@
+import {Bicycle} from '../../app/bicycle';
+
+
+export const BICYCLES: Bicycle[] = [
+    {
+        model: 'SX1723',
+        color: 'Blue',
+        company: 'Shwinn'   
+    },
+    {
+        model: 'CD82736',
+        color: 'Yellow',
+        company: 'Cannondale'   
+    },
+    {
+        model: 'X-71',
+        color: 'Red',
+        company: 'BMX'   
+    },
+    {
+        model: 'Outdoorsman',
+        color: 'Green',
+        company: 'Raleigh'   
+    },
+    
+];
